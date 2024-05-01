b8d91e2f8f39f94af8ee9a879ec49768a82d7b0d
Tony Wu <tonytlwu@users.noreply.github.com>
2024-05-01 18:19:47 +0100

    Reorder configuration fields to have some logical consistency with dynamic image and record container

--- js/interface.js
+++ js/interface.js
@@ -31,39 +31,6 @@ function toggleFilterField(filter, fieldName, value, compareValue) {
     fields: [
       {
         type: 'provider',
-        name: 'clickAction',
-        package: 'com.fliplet.link',
-        data: function(value) {
-          return _.assign({}, value, {
-            options: {
-              actionLabel: 'Click action'
-            }
-          });
-        }
-      },
-      {
-        type: 'radio',
-        name: 'updateType',
-        label: 'Select mode for reactivity to data changes',
-        required: true,
-        default: 'none',
-        options: [
-          {
-            value: 'none',
-            label: '<strong>No Update</strong> - Updates are not silently applied and users won\'t see the changes until they load the list for the next time.'
-          },
-          {
-            value: 'informed',
-            label: '<strong>Informed Update</strong> - Users are informed if an update is available. When the user chooses to apply it, changes are applied in-situ, i.e. without a complete reload.'
-          },
-          {
-            value: 'live',
-            label: '<strong>Real-time Update</strong> - Updates are automatically applied when they are available. Detail view can be directly loaded (via query parameter) without loading the list first.'
-          }
-        ]
-      },
-      {
-        type: 'provider',
         package: 'com.fliplet.data-source-provider',
         data: function() {
           return Fliplet.Widget.findParents({ filter: { package: 'com.fliplet.dynamic-container' } }).then((widgets) => {
@@ -77,41 +44,6 @@ function toggleFilterField(filter, fieldName, value, compareValue) {
             };
           });
         }
-      },
-      {
-        name: 'sorts',
-        type: 'list',
-        label: 'How do you want to sort your data?',
-        addLabel: 'Add sort condition',
-        headingFieldName: 'field',
-        fields: [
-          {
-            name: 'field',
-            type: 'dropdown',
-            label: 'Data field',
-            required: true,
-            placeholder: '-- Select a column',
-            options: dataSourceColumns
-          },
-          {
-            name: 'order',
-            type: 'dropdown',
-            label: 'Sort order',
-            required: true,
-            placeholder: false,
-            default: 'asc',
-            options: [
-              {
-                label: 'Ascending',
-                value: 'asc'
-              },
-              {
-                label: 'Descending',
-                value: 'desc'
-              }
-            ]
-          }
-        ]
       },
       {
         name: 'filters',
@@ -250,6 +182,74 @@ function toggleFilterField(filter, fieldName, value, compareValue) {
             required: true,
             type: 'text',
             placeholder: 'Enter app storage key'
+          }
+        ]
+      },
+      {
+        name: 'sorts',
+        type: 'list',
+        label: 'How do you want to sort your data?',
+        addLabel: 'Add sort condition',
+        headingFieldName: 'field',
+        fields: [
+          {
+            name: 'field',
+            type: 'dropdown',
+            label: 'Data field',
+            required: true,
+            placeholder: '-- Select a column',
+            options: dataSourceColumns
+          },
+          {
+            name: 'order',
+            type: 'dropdown',
+            label: 'Sort order',
+            required: true,
+            placeholder: false,
+            default: 'asc',
+            options: [
+              {
+                label: 'Ascending',
+                value: 'asc'
+              },
+              {
+                label: 'Descending',
+                value: 'desc'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        type: 'provider',
+        name: 'clickAction',
+        package: 'com.fliplet.link',
+        data: function(value) {
+          return _.assign({}, value, {
+            options: {
+              actionLabel: 'Click action'
+            }
+          });
+        }
+      },
+      {
+        type: 'radio',
+        name: 'updateType',
+        label: 'Select mode for reactivity to data changes',
+        required: true,
+        default: 'none',
+        options: [
+          {
+            value: 'none',
+            label: '<strong>No Update</strong> - Updates are not silently applied and users won\'t see the changes until they load the list for the next time.'
+          },
+          {
+            value: 'informed',
+            label: '<strong>Informed Update</strong> - Users are informed if an update is available. When the user chooses to apply it, changes are applied in-situ, i.e. without a complete reload.'
+          },
+          {
+            value: 'live',
+            label: '<strong>Real-time Update</strong> - Updates are automatically applied when they are available. Detail view can be directly loaded (via query parameter) without loading the list first.'
           }
         ]
       },
